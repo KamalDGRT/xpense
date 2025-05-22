0084c3bce87492371966cde417cff2f55da354f6
KamalDGRT <kamaldgrt@gmail.com>
2025-05-23 01:21:37 +0530

    base project setup

---
+++ XPense/XPenseApp.swift
@@ -0,0 +1,15 @@
+//
+// XPenseApp.swift
+// XPense
+//
+
+import SwiftUI
+
+@main
+struct XPenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
